486088257ba8664990f3b17b891ca3562ea1d321
Alex Duggan <aldug@src.gnome.org>
2002-09-17 00:46:23 +0000

    first attempt

--- show_tip.php
+++ show_tip.php
@@ -7,16 +7,15 @@ if($tipID)
 	include("header.inc.php");
 	create_middle_box_top("tips");
 	
-   $tip_select_result = mysql_query("SELECT title,body,thumbnail_filename FROM tip WHERE tipID='$tipID'");
+   $tip_select_result = mysql_query("SELECT title,body, FROM tip WHERE tipID='$tipID'");
 	if(mysql_num_rows($tip_select_result)==0)
    {
    	print("Invalid Tip.");
    }
    else
    {
-   	list($title,$body,$thumbnail_filename) = mysql_fetch_row($tip_select_result);
+   	list($title,$body) = mysql_fetch_row($tip_select_result);
       print("<span class=\"yellow-text\"><font size=\"+1\">$title</font></span>\n");
-      print("<p><div align=\"center\"><img src=\"images/thumbnails/tips/$thumbnail_filename\"></div>\n");
       print("<p>\n$body");
    }
 
