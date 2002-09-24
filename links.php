18feeab3119662a08bdad011dc7e6bbf7bf8a0c6
Alex Duggan <aldug@src.gnome.org>
2002-09-24 00:08:50 +0000

    reworked links page

--- links.php
+++ links.php
@@ -7,40 +7,57 @@ include("header.inc.php");
 create_middle_box_top("links");
 
 print("<table border=\"0\">\n");
-print("<tr><td><img src=\"images/site/gnome-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">GNOME-Links</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/gnome-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">GNOME Links</a></td></tr>\n");
 print("<tr><td>&nbsp;</td><td>");
 print("<table border=\"0\">\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gnome.org\">GNOME Project Homepage</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gnomedesktop.org\">GNOME Desktop News</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gnomesupport.org\">GNOME Support</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.ximian.com\">Ximian Inc. Homepage</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.codefactory.se\">Codefactory Homepage</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.redhat.com\">RedHat Homepage</a></td></tr>\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gnomedesktop.org\">GNOME Desktop News</a></td></tr>\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://g-scripts.sourceforge.net\">Nautilus Script Collection</a></td></tr>\n");
+
 print("</table>\n");
 print("</td></tr>\n");
 print("</table>\n<p>\n");
 
 print("<table border=\"0\">\n");
-print("<tr><td><img src=\"images/site/art-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">ART-Links</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/dude-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">GNOME Dudes</a></td></tr>\n");
 print("<tr><td>&nbsp;</td><td>");
 print("<table border=\"0\">\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.astrolinux.com\">Aldug's Homepage</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.whitecape.org\">Dekars's Homepage</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.hadess.net\">Hadess's Homepage</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://jimmac.musichall.cz\">Jimmac's Homepage</a></td></tr>\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.linuxart.com\">Linuxart</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.linuxart.com\">Garrett's Homepage</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://tigert.gimp.org\">Tigert's Homepage</a></td></tr>\n");
 print("</table>\n");
 print("</td></tr>\n");
 print("</table>\n<p>\n");
 
 print("<table border=\"0\">\n");
-print("<tr><td><img src=\"images/site/www-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">OTHER-Links</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/art-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">Theme Links</a></td></tr>\n");
 print("<tr><td>&nbsp;</td><td>");
 print("<table border=\"0\">\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gkrellm.net\">GKrellM</a></td></tr>\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://muhri.net/\">Muhri.net</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.themedepot.org\">Theme Depot</a></td></tr>\n");
 print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://themes.freshmeat.net\">Themes.org</a></td></tr>\n");
-print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.xmms.org\">XMMS</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://galeon.sourceforge.net/themes/themes.php\">Galeon Themes</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://gnomeicu.sourceforge.net/iconthemes.php\">Gnomeicu Themes</a></td></tr>\n");
+
+
+print("</table>\n");
+print("</td></tr>\n");
+print("</table>\n<p>\n");
+
+print("<table border=\"0\">\n");
+print("<tr><td><img src=\"images/site/www-icon.png\"></td><td><font size=\"+1\" class=\"yellow-text\">Other Links</a></td></tr>\n");
+print("<tr><td>&nbsp;</td><td>");
+print("<table border=\"0\">\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gnome.org/projects/nautilus/\">Nautilus Homepage</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://g-scripts.sourceforge.net\">Nautilus Script Collection</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.gkrellm.net\">GKrellM</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://themes.freshmeat.net\">Themes.org</a></td></tr>\n");
+print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"http://www.xmms.org\">X Multimedia System (XMMS)</a></td></tr>\n");
 print("</table>\n");
 print("</td></tr>\n");
 print("</table>\n<p>\n");
