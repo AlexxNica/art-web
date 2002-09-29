0e26e722d641a4c744ad19ca1c9b3f278c3b4b99
Alex Duggan <aldug@src.gnome.org>
2002-09-29 15:52:09 +0000

    improve words in drop down menu

--- submit_theme.php
+++ submit_theme.php
@@ -10,7 +10,7 @@ print("If you would like to submit your theme to art.gnome.org, please fill out 
 print("<form action=\"$PHP_SELF\" method=\"post\">\n");
 print("<table border=\"0\">");
 print("<tr><td><b>Theme Name:</b></td><td><input type=\"text\" name=\"theme_name\" size=\"40\"></td></tr>\n");
-print("<tr><td><b>Category</b></td><td><select name=\"category\"><option value=\"gdm_greeter\">gdm_greeter<option value=\"gtk\">gtk<option value=\"gtk2\">gtk2<option value=\"metacity\">metacity<option value=\"metatheme\">metatheme<option value=\"nautilus\">nautilus<option value=\"sawfish\">sawfish<option value=\"sounds\">sounds<option value=\"splash_screens\">splash_screens</select></td></tr>\n");
+print("<tr><td><b>Category</b></td><td><select name=\"category\"><option value=\"gdm_greeter\">GDM Greeter<option value=\"gtk\">GTK+ 1.2<option value=\"gtk2\">GTK+ 2.0<option value=\"metacity\">Metacity<option value=\"metatheme\">Metatheme<option value=\"nautilus\">Nautilus<option value=\"sawfish\">Sawfish<option value=\"sounds\">Sounds<option value=\"splash_screens\">Splash Screens</select></td></tr>\n");
 print("<tr><td><b>Theme Author:</b></td><td><input type=\"text\" name=\"theme_author\" size=\"40\"></td></tr>\n");
 print("<tr><td><b>Author Email:</b></td><td><input type=\"text\" name=\"author_email\" size=\"40\"></td></tr>\n");
 print("<tr><td><b>URL of Theme:</b></td><td><input type=\"text\" name=\"theme_url\" size=\"40\"></td></tr>\n");
