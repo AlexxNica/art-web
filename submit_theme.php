426396f124dcf23066c394a14604a27f06ef9eff
Alex Duggan <aldug@src.gnome.org>
2002-09-29 15:36:56 +0000

    fixed html error

--- submit_theme.php
+++ submit_theme.php
@@ -7,6 +7,7 @@ include("header.inc.php");
 create_middle_box_top("themes");
 
 print("<form action=\"$PHP_SELF\" method=\"post\">\n");
+print("<table border=\"0\">");
 print("<tr><td><b>Theme Name:</b></td><td><input type=\"text\" name=\"theme_name\" size=\"40\"></td></tr>\n");
 print("<tr><td><b>Category</b></td><td><select name=\"category\"><option value=\"gdm_greeter\">gdm_greeter<option value=\"gtk\">gtk<option value=\"gtk2\">gtk2<option value=\"metacity\">metacity<option value=\"metatheme\">metatheme<option value=\"nautilus\">nautilus<option value=\"sawfish\">sawfish<option value=\"sounds\">sounds<option value=\"splash_screens\">splash_screens</select></td></tr>\n");
 print("<tr><td><b>Theme Author:</b></td><td><input type=\"text\" name=\"theme_author\" size=\"40\"></td></tr>\n");
