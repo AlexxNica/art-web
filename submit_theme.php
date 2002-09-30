f8d8c10497a3c26780a9a48e54273c2154ea9aca
Alex Duggan <aldug@src.gnome.org>
2002-09-30 17:00:30 +0000

    make the form actually work, fix 1

--- submit_theme.php
+++ submit_theme.php
@@ -38,7 +38,7 @@ else
 	print("<tr><td><b>Theme Author:</b></td><td><input type=\"text\" name=\"theme_author\" size=\"40\"></td></tr>\n");
 	print("<tr><td><b>Author Email:</b></td><td><input type=\"text\" name=\"author_email\" size=\"40\"></td></tr>\n");
 	print("<tr><td><b>URL of Theme:</b></td><td><input type=\"text\" name=\"theme_url\" size=\"40\"></td></tr>\n");
-	print("<tr><td><b>Description:</b></td><td><textarea name=\"description\" cols=\"40\" rows=\"5\" wrap></textarea></td></tr>\n");
+	print("<tr><td><b>Description:</b></td><td><textarea name=\"theme_description\" cols=\"40\" rows=\"5\" wrap></textarea></td></tr>\n");
 	print("</table>\n<p>\n");
 	print("<input type=\"submit\" value=\"Submit Theme\">\n"); 
 	print("</form>\n");
