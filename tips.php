d620da20cc5506309948b23a6a5cba6c806f3167
Alex Duggan <aldug@src.gnome.org>
2002-10-10 22:30:59 +0000

    allow for outside links in tips and tricks

--- tips.php
+++ tips.php
@@ -6,7 +6,7 @@ require("common.inc.php");
 include("header.inc.php");
 create_middle_box_top("tips");
 
-$tip_select_result = mysql_query("SELECT tipID,title FROM tip");
+$tip_select_result = mysql_query("SELECT tipID,type,title,tip_url FROM tip");
 if(mysql_num_rows($tip_select_result)==0)
 {
 	print("In the future, this section will contain cool tips and tricks for the gnome desktop");
@@ -14,10 +14,17 @@ if(mysql_num_rows($tip_select_result)==0)
 else
 {
 	print("<table border=\"0\">\n");
-   while(list($tipID,$title) = mysql_fetch_row($tip_select_result))
+   while(list($tipID,$type,$title,$tip_url) = mysql_fetch_row($tip_select_result))
 	{
-		print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"show_tip.php?tipID=$tipID\">$title</a></td></tr>\n");
-	}
+		if($type == "normal")
+      {
+      	print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"show_tip.php?tipID=$tipID\">$title</a></td></tr>\n");
+		}
+      else
+      {
+      	print("<tr><td><img src=\"images/site/circle.png\"></td><td>&nbsp;<a href=\"$tip_url\">$title</a></td></tr>\n");
+		}
+   }
 	print("</table>\n");
 }
 
