5f2478d0c72ec7e1f0d83f8d8ffc98447b6a2d53
Alex Duggan <aldug@gnome.org>
2002-12-21 16:59:11 +0000

    two new functions to diplay background and theme updates,
    2002-12-21  Alex Duggan  <aldug@gnome.org>
    
    	* includes/common.inc.php: two new functions to diplay background and theme updates,
    	print_background_row(), print_theme_row()
    	* updates.php: now use print_background_row() and print_theme_row()

--- updates.php
+++ updates.php
@@ -20,18 +20,11 @@ for($count=0;$count<count($big_array);$count++)
 	list($add_timestamp,$type,$ID) = explode("|",$big_array[$count]);
 	if($type == "background")
    {
-   	$background_select_result = mysql_query("SELECT background_name, category, author,release_date,thumbnail_filename FROM background WHERE backgroundID='$ID'");
-   	list($background_name,$category,$author,$release_date,$thumbnail_filename) = mysql_fetch_row($background_select_result);
-      $release_date = fix_sql_date($release_date,"/");
-      print("<tr><td><a href=\"show_background.php?backgroundID=$ID&category=$category\"><img src=\"images/thumbnails/backgrounds/$thumbnail_filename\" border=\"0\"></td><td><a class=\"screenshot\" href=\"show_background.php?backgroundID=$ID&category=$category\">$background_name</a><br>$release_date<br>BACKGROUNDS - GNOME<br>$author</td></tr>\n");
-   }
+   	print_background_row($ID);
+	}
    else
    {
-   	$theme_select_result = mysql_query("SELECT theme_name, category, author, release_date,small_thumbnail_filename FROM theme WHERE themeID='$ID'");
-   	list($theme_name,$category,$author,$release_date,$thumbnail_filename) = mysql_fetch_row($theme_select_result);
-      $release_date = fix_sql_date($release_date,"/");
-      $category_good = $linkbar["themes_" . $category]["alt"];
-      print("<tr><td><a href=\"show_theme.php?themeID=$ID&category=$category\"><img src=\"images/thumbnails/$category/$thumbnail_filename\" border=\"0\"></a></td><td><a class=\"screenshot\" href=\"show_theme.php?themeID=$ID&category=$category\">$theme_name</a><br>$release_date<br>THEMES - $category_good<br>$author</td></tr>\n");
+   	print_theme_row($ID);
 	}
 }
 print("</table>\n");
