6bb67ea6e6fd02c989e2147760ab7861a617b4ef
Alex Duggan <aldug@gnome.org>
2002-12-26 19:17:53 +0000

    include at the top of the page a list of questions with local links to the
    2002-12-26  Alex Duggan  <aldug@gnome.org>
    
    	* faq.php: include at the top of the page a list of questions with local links
    	to the question and answer below in the page

--- faq.php
+++ faq.php
@@ -8,17 +8,26 @@ include("header.inc.php");
 
 create_middle_box_top("faq");
 
-$faq_select_result = mysql_query("SELECT question,answer FROM faq ORDER by faqID");
+$faq_select_result = mysql_query("SELECT faqID,question,answer FROM faq ORDER by faqID");
 if(mysql_num_rows($faq_select_result)==0)
 {
 	print("In the future, this site will contain a FAQ.");
 }
 else
 {
+	/* print out a list of questions */
 	print("<ol>\n");
-   while(list($question,$answer)=mysql_fetch_row($faq_select_result))
+	while(list($faqID,$question,$answer)=mysql_fetch_row($faq_select_result))
    {
-   	print("<li><span class=\"yellow-text\">$question</span>\n<p>$answer\n\n");
+   	print("<li><a href=\"$PHP_SELF#$faqID\">$question</a>\n");
+   }
+	print("</ol>\n<p>\n<ol>\n");
+	
+   /* print out a list of questions and answers */
+	$faq_select_result = mysql_query("SELECT faqID,question,answer FROM faq ORDER by faqID");
+	while(list($faqID,$question,$answer)=mysql_fetch_row($faq_select_result))
+   {
+   	print("<li><a name=\"$faqID\"></a><span class=\"yellow-text\">$question</span>\n<p>$answer\n\n");
    }
    print("</ol>\n");
 }
