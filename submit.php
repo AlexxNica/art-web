9b580985a2bcc54eb7ee7d4a39ecf2802985afc9
Alex Duggan <aldug@astrolinux.com>
2003-04-11 00:17:54 +0000

    remove extranious period. remove link to metathemes.
    2003-04-10  Alex Duggan  <aldug@astrolinux.com>
    
    	* submit.php: remove extranious period.
    	* themes.php: remove link to metathemes.

--- submit.php
+++ submit.php
@@ -10,7 +10,7 @@ ago_header("SUBMIT");
 create_middle_box_top("submit");
 
 print("If you would like to submit your background or theme to art.gnome.org, please use the links below.  If you are submitting a background, please make sure that it is a GNOME related background.");
-print("<p><ul>\n<li><a class=\"screenshot\" href=\"submit_background.php\">Background submission form</a>\n<li><a class=\"screenshot\" href=\"submit_theme.php\">Theme submission form</a>\n</ul>\n.");
+print("<p><ul>\n<li><a class=\"screenshot\" href=\"submit_background.php\">Background submission form</a>\n<li><a class=\"screenshot\" href=\"submit_theme.php\">Theme submission form</a>\n</ul>\n");
 
 create_middle_box_bottom();
 ago_footer();
