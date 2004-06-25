260f2ad604c86bbbac4c4a625703e1c26fa42157
Thomas Wood <thos@gnome.org>
2004-06-25 12:51:45 +0000

    Initial commit
    2004-06-23  Thomas Wood <thos@gnome.org>
            * admin/delete_faq.php: Initial commit

---
+++ admin/delete_faq.php
@@ -0,0 +1,61 @@
+<?php
+require("mysql.inc.php");
+require("common.inc.php");
+
+// ensure POST special characters are escaped, regardless of magic_quotes_gpc setting
+escape_gpc_array ($_POST);
+
+// Extracts the POST variables to global variables
+// Not ideal solution, but easiest
+extract($_POST, EXTR_SKIP);
+
+print("<html>\n<head><title>Delete a FAQ</title></head>\n<body>\n");
+print("<div align=\"center\">");
+print("<font size=\"+2\">Delete a FAQ</font>\n<p>\n");
+print("</div>\n");
+
+// write the updated background text do the database
+if($action == "delete")
+{
+    /* remove from faq database */
+    $faq_delete_query = "DELETE FROM faq WHERE faqID='$faqID'";
+    $faq_delete_result = mysql_query($faq_delete_query);
+    
+    if($faq_delete_result)
+    {
+        print("Sucessfully deleted FAQ.");
+    }
+    else
+    {
+        print("Error deleting FAQ.");
+    }
+    print("Click <a href=\"$PHP_SELF\">here</a> to return");
+}
+// display the confirmation window
+elseif($action == "confirm")
+{
+    $faq_select_result = mysql_query("SELECT question FROM faq WHERE faqID='$faqID'");
+    list($question) = mysql_fetch_row($faq_select_result);
+    print("Are you sure you want to delete \"$question\" (faqID: $faqID) from the database?");
+    print("<p>\n");
+    print("<form action=\"$PHP_SELF\" method=\"post\">\n");
+    print("<input type=\"submit\" value=\"Continue\">\n");
+    print("<input type=\"hidden\" name=\"faqID\" value=\"$faqID\">\n");
+    print("<input type=\"hidden\" name=\"action\" value=\"delete\">\n");
+    print("</form>\n");
+}
+else
+{
+    $faq_select_result = mysql_query("SELECT faqID,question FROM faq ORDER by faqID");
+    print("<form action=\"$PHP_SELF\" method=\"post\">\n");
+    print("<select name=\"faqID\" size=\"5\">\n");
+    while(list($faqID,$question) = mysql_fetch_row($faq_select_result))
+    {
+        print("<option value=\"$faqID\">$question\n");
+    }
+    print("</select><input type=\"submit\" value=\"Delete\">");
+    print("<input type=\"hidden\" name=\"action\" value=\"confirm\">\n</form>\n");
+}
+print("</body>\n</html>\n");
+?>
+    
