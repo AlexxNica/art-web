993bc0f195b1223239f7ac0b1466ce595820add9
Thomas Wood <thos@gnome.org>
2004-10-09 18:03:36 +0000

    Added end user submission tracking
    2004-10-09  Thomas Wood  <thos@gnome.org>
    
            * submit_background.php, submit_theme.php: Added end user submission
            tracking

--- submit_background.php
+++ submit_background.php
@@ -16,6 +16,7 @@ $author_email = $_POST["author_email"];
 $background_url = $_POST["background_url"];
 $background_screenshot_url = $_POST["background_screenshot_url"];
 $background_description = $_POST["background_description"];
+$submission_id = $_POST["submission_id"];
 
 ago_header("Background Submission");
 create_middle_box_top("backgrounds");
@@ -23,24 +24,40 @@ create_middle_box_top("backgrounds");
 if($_POST)
 {
 	if($background_name && $category && $background_author && $author_email && $background_url && $background_description)
-   {
-   	$incoming_background_insert_query  = "INSERT INTO incoming_background(backgroundID,status,background_name,category,author,author_email,background_url,background_screenshot_url,background_description) ";
-   	$incoming_background_insert_query .= "VALUES('','new','$background_name','$category','$background_author','$author_email','$background_url','$background_screenshot_url','$background_description')";
-   	$incoming_background_insert_result = mysql_query("$incoming_background_insert_query");
-      if(mysql_affected_rows()==1)
-      {
-      	print("Thank you, your background will be considered for inclusion in art.gnome.org");
-      }
-      else
-      {
-      	print("There were form submission errors, please try again.");
-      }
-   }
-   else
-   {
-   	print("Error, you must fill out all of the previous form fields, please go back and try again.");
-   }
-	print("<p>\n");
+	{
+		$incoming_background_insert_query = "INSERT INTO incoming_background(backgroundID,status,background_name,category,author,author_email,background_url,background_screenshot_url,background_description) ";
+		$incoming_background_insert_query .= "VALUES('','new','$background_name','$category','$background_author','$author_email','$background_url','$background_screenshot_url','$background_description')";
+		$incoming_background_insert_result = mysql_query("$incoming_background_insert_query");
+		if(mysql_affected_rows()==1)
+		{
+			print("Thank you, your background will be considered for inclusion in art.gnome.org");
+		}
+		else
+		{
+			print("There were form submission errors, please try again.");
+		}
+	}
+	else
+	{
+		if ($submission_id)
+		{
+			$submission_result = mysql_query("SELECT background_name,status FROM incoming_background WHERE backgroundID = $submission_id");
+			list ($submission_name,$submission_status) = mysql_fetch_row($submission_result);
+			print("<b>Submission Status</b><p>");
+			switch ($submission_status)
+			{
+				case "new" : print("The background &quot;$submission_name&quot; is currently pending intial review."); break;
+				case "added" : print("&quot;$submission_name&quot; has been added to the art.gnome.org database and should be available through the site."); break;
+				case "rejected" : print("The background &quot;$submission_name&quot; has been removed from the submissions list and not been added to the site. This may happen if the background was inappropriate to art.gnome.org, appeared to be incomplete or unfinished, or could not be retrieved from the URL provided. If you have any queries regarding this status, please contact a memeber of the art.gnome.org team, quoting the submission tracking ID."); break;
+				default: print("There was an error retrieving the status of the background submission ID you requested. Please check and try again.");
+			}
+		print("</p><a href=\"$PHP_SELF\">Back to Background Submission Page</a>");
+		}
+		else
+		{
+			print("Error, you must fill out all of the previous form fields, please go back and try again.");
+		}
+	}
 }
 else
 {
@@ -58,6 +75,13 @@ else
 	print("</table>\n<p>\n");
 	print("<input type=\"submit\" value=\"Submit Background\">\n");
 	print("</form>");
+
+
+	print("<hr /><b>Submission tracking</b>");
+	print("<p><form action=\"$PHP_SELF\" method=\"post\">");
+	print("Submission ID: <input name=\"submission_id\" />");
+	print("<input type=\"submit\" value=\"Get Status\" /></form></p>");
+
 }
 
 create_middle_box_bottom();
--- submit_theme.php
+++ submit_theme.php
@@ -15,6 +15,7 @@ $theme_author = $_POST["theme_author"];
 $author_email = $_POST["author_email"];
 $theme_url = $_POST["theme_url"];
 $theme_description = $_POST["theme_description"];
+$submission_id = $_POST["submission_id"];
 
 ago_header("Theme Submission");
 create_middle_box_top("themes");
@@ -22,24 +23,42 @@ create_middle_box_top("themes");
 if($_POST)
 {
 	if($theme_name && $category && $theme_author && $author_email && $theme_url && $theme_description)
-   {
-   	$incoming_theme_insert_query  = "INSERT INTO incoming_theme(themeID,status,theme_name,category,author,author_email,theme_url,theme_description) ";
-   	$incoming_theme_insert_query .= "VALUES('','new','$theme_name','$category','$theme_author','$author_email','$theme_url','$theme_description')";
-   	$incoming_theme_insert_result = mysql_query("$incoming_theme_insert_query");
-      if(mysql_affected_rows()==1)
-      {
-      	print("Thank you, your theme will be considered for inclusion in art.gnome.org");
-      }
-      else
-      {
-      	print("There were form submission errors, please try again.");
-      }
-   }
-   else
-   {
-   	print("Error, you must fill out all of the previous form fields, please go back and try again.");
-   }
-	print("<p>\n");
+	{
+		$incoming_theme_insert_query  = "INSERT INTO incoming_theme(themeID,status,theme_name,category,author,author_email,theme_url,theme_description) ";
+		$incoming_theme_insert_query .= "VALUES('','new','$theme_name','$category','$theme_author','$author_email','$theme_url','$theme_description')";
+		$incoming_theme_insert_result = mysql_query("$incoming_theme_insert_query");
+		if(mysql_affected_rows()==1)
+		{
+			print("Thank you, your theme will be considered for inclusion in art.gnome.org.<br />");
+			$id = mysql_insert_id();
+			print("Your theme submission tracking ID is $id. Use this to track the status of your submission and in any queries regarding your submission status.");
+		}
+		else
+		{
+			print("There were form submission errors, please try again.");
+		}
+	}
+	else
+	{
+		if ($submission_id)
+		{
+			$submission_result = mysql_query("SELECT theme_name,status FROM incoming_theme WHERE themeID = $submission_id");
+			list ($submission_name,$submission_status) = mysql_fetch_row($submission_result);
+			print("<b>Submission Status</b><p>");
+			switch ($submission_status)
+			{
+				case "new" : print("The theme &quot;$submission_name&quot; is currently pending intial review."); break;
+				case "added" : print("&quot;$submission_name&quot; has been added to the art.gnome.org database and should be available through the site."); break;
+				case "rejected" : print("The theme &quot;$submission_name&quot; has been removed from the submissions list and not been added to the site. This may happen if the theme was inappropriate to art.gnome.org, appeared to be incomplete or unfinished, or could not be retrieved from the URL provided. If you have any queries regarding this status, please contact a memeber of the art.gnome.org team, quoting the submission tracking ID."); break;
+				default: print("There was an error retrieving the status of the theme submission ID you requested. Please check and try again.");
+			}
+			print("</p><a href=\"$PHP_SELF\">Back to Theme Submission Page</a>");
+		}
+		else
+		{
+			print("Error, you must fill out all of the previous form fields, please go back and try again.");
+		}
+	}
 }
 else
 {
@@ -55,6 +74,11 @@ else
 	print("</table>\n<p>\n");
 	print("<input type=\"submit\" value=\"Submit Theme\">\n"); 
 	print("</form>\n");
+
+	print("<hr /><b>Submission tracking</b>");
+	print("<p><form action=\"$PHP_SELF\" method=\"post\">");
+	print("Submission ID: <input name=\"submission_id\" />");
+	print("<input type=\"submit\" value=\"Get Status\" /></form></p>");
 }
 
 create_middle_box_bottom();
