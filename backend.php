d6aa816a80da196de9d56b5f523d11c4ba94663f
Bruno Santos <bmfs@eksperimental.net>
2007-05-27 16:40:44 +0000

    Deleted Everything from AGOv3 branch
    2007-05-27 Bruno Santos <bmfs@eksperimental.net>
    
      * Deleted Everything from AGOv3 branch
    
    
    
    svn path=/branches/art-web-3/; revision=463

--- backend.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Redirect to updates.php now that it can do exactly the same as this page */
-
-if ($_SERVER['HTTPS']) $protocol = 'https'; else $protocol = 'http';
-header ("Location: $protocol://{$_SERVER['SERVER_NAME']}/updates.php?format=rss");
-?>
