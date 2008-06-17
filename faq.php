c98c9a3a1f125358e707616607efa7e072ba7898
Thomas Wood <thos@src.gnome.org>
2008-06-17 19:56:30 +0000

    Convert faq.php to use template system
    * Convert faq.php to use template system
    
    svn path=/branches/art-hub/; revision=536

--- faq.php
+++ faq.php
@@ -1,59 +1,10 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html><head>
+<?php
+include ("lib/template.php");
 
 
-  <link href="css/layout.css" rel="stylesheet" type="text/css" media="screen">
-  <link href="css/style.css" rel="stylesheet" type="text/css" media="all">
-  <link rel="icon" type="image/png" href="http://www.gnome.org/img/logo/foot-16.png">
-  <link rel="SHORTCUT ICON" type="image/png" href="http://www.gnome.org/img/logo/foot-16.png">
-  <title>GNOME: The Free Software Desktop Project</title>
-
-  <style type="text/css">
-    tt {background-color:#FFFFBF;}
-  </style>
-
-  <link rel="stylesheet" type="text/css" href="css/frontpage.css"></head><body>
-  <!-- site header -->
-  <div id="page">
-    <ul id="general">
-      <li id="siteaction-gnome_home" class="home">
-        <a href="http://www.gnome.org/" title="Home">Home</a>
-      </li>
-      <li id="siteaction-gnome_news">
-        <a href="http://news.gnome.org/" title="News">News</a>
-      </li>
-      <li id="siteaction-gnome_projects">
-        <a href="http://www.gnome.org/projects/" title="Projects">Projects</a>
-      </li>
-      <li id="siteaction-gnome_art">
-        <a href="http://art.gnome.org/" title="Art">Art</a>
-      </li>
-      <li id="siteaction-gnome_support">
-        <a href="http://www.gnome.org/support/" title="Support">Support</a>
-      </li>
-      <li id="siteaction-gnome_development">
-        <a href="http://developer.gnome.org/" title="Development">Development</a>
-      </li>
-      <li id="siteaction-gnome_community">
-        <a href="http://www.gnome.org/community/" title="Community">Community</a>
-      </li>
-    </ul>
-    <div id="header">
-      <h1>GNOME Art</h1>
-      <div id="tabs">
-        <ul id="portal-globalnav">
-          <li><a href="/"><span>Art</span></a></li>
-          <li><a href="/themes"><span>Themes</span></a></li>
-          <li><a href="/backgrounds"><span>Backgrounds</span></a></li>
-          <li class="selected"><a href="/faq"><span>FAQ</span></a></li>
-        </ul>
-      </div> <!-- end of #tabs -->
-    </div> <!-- end of #header -->
-  </div>
-<!-- end site header -->
-
-  <div id="body">
-    <div id="content">
+$t = new Template ("faq");
+$t->print_header ();
+?>
 
 <div id="bottom">
   <div id="bottomLeft">
@@ -132,15 +83,4 @@
 </div>
 
 
-
-  </div> <!-- end of div#content -->
-
-  <div id="footer">
-    Copyright &copy; 2005-2008 <a href="http://www.gnome.org/">The GNOME Project</a>.<br>
-    <a href="http://validator.w3.org/check/referer">Optimised</a> for <a
-    href="http://www.w3.org/">standards</a>. Hosted by <a
-    href="http://www.redhat.com/">Red Hat</a>.
-  </div>
-
-  </div> <!-- end of div#body -->
-</body></html>
+<?php $t->print_footer(); ?>
