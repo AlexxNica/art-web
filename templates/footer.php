81cddf6f03ded0deccd097623ee017386d313019
Thomas Wood <thos@gnome.org>
2009-11-04 21:24:51 +0000

    Update copyright and source code statements

--- templates/footer.php
+++ templates/footer.php
@@ -2,9 +2,10 @@
 </div> <!-- end of div#content -->
 
   <div id="footer">
-    Copyright &copy; 2005-2008 <a href="http://www.gnome.org/">The GNOME Project</a>.<br>
+    Copyright &copy; 2005-2009 <a href="http://www.gnome.org/">The GNOME Project</a>.<br>
     Hosted by <a href="http://www.redhat.com/">Red Hat</a>.
-    Source code is available in <a href="http://svn.gnome.org/svn/art-web/">GNOME SVN</a>.
+    Source code available in the <a href="http://git.gnome.org/cgit/art-web/">
+    GIT repository</a>.
   </div>
 
   </div> <!-- end of div#body -->
